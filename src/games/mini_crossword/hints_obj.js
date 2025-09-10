aca7c6948fa98ad3416b35729145b9d0a25ffc21
pratikkharade <53163485+pratikkharade@users.noreply.github.com>
2025-09-09 22:04:55 -0500

    Updated mini - 09/10

--- src/games/mini_crossword/hints_obj.js
+++ src/games/mini_crossword/hints_obj.js
@@ -1,17 +1,17 @@
 const hints_obj = {
     across: [
-        "1A : On this spot",
-        "5A : =",
-        "7A : Organisms that sound like someone you'd enjoy hanging out with",
-        "8A : Where the North Base Camp for Mount Everest can be found",
-        "9A : Combustible funeral structure"
+        "1A : What a phisher might try to get you to click",
+        "5A : \"___ Copperhead,\" Barbara Kingsolver novel that won a 2023 Pulitzer",
+        "6A : Act melodramatically",
+        "7A : One just squeaking by?",
+        "8A : Suspicious of",
     ],
     down: [
-        "1D : Serious weight",
-        "2D : Furnish with supplies",
-        "3D : Bring up to in order to get a quick opinion",
-        "4D : Like the proverbial beaver",
-        "6D : Miller ___ (beer)"
+        "1D : Fruit used to treat scurvy",
+        "2D : \"Catch ya later!\"",
+        "3D : \"Quite the contrary!\"",
+        "4D : Bending body part",
+        "5D : Music submission to a recording studio"
     ]
 }
 
